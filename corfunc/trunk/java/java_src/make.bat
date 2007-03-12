e19c5bbfd80c464d2507038e5577d465f2e67a98
Jonathan Rawle <jonathan.rawle@diamond.ac.uk>
2007-03-12 11:32:31 +0000

    corfunc: Add Makefiles for compilation on Win/GNU

--- corfunc/trunk/java/java_src/make.bat
+++
@@ -1,5 +0,0 @@
-javac -classpath .\;U:\corfunc\corfunc_src\java\java_src\other_jars\jh.jar;U:\corfunc\corfunc_src\java\java_src\other_jars\jai_core.jar;U:\corfunc\corfunc_src\java\java_src\other_jars\jai_codec.jar;U:\corfunc\corfunc_src\java\java_src\other_jars\graph.jar *.java
-jar cmf main.txt corfunc.jar *.class
-copy U:\corfunc\corfunc_src\java\java_src\Corfunc.jar U:\corfunc\corfunc_src
-del U:\corfunc\corfunc_src\java\java_src\Corfunc.jar
-copy U:\corfunc\corfunc_src\java\java_src\other_jars\*.jar U:\corfunc\corfunc_src\*.jar
